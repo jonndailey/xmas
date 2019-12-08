794df173f3d7224199c42bacbb62d4ab9787eaa8
Jonny Dailey <jonndailey@gmail.com>
2019-12-07 20:02:14 -0500

    Update xmas.py
    Changed the order of operations to properly print "Merry Christmas" when the value 15 is reached.

--- xmas.py
+++ xmas.py
@@ -1,9 +1,3 @@
-# X-Mas
-# Aeriel Denmark
-# Fri 12/6/19
-
-#
-
 import math
 
 myArray = range(1,16)
@@ -11,11 +5,12 @@ myArray = range(1,16)
 print(myArray)
 
 for i in myArray:
-	if i % 3 == 0:
-		print("Merry")
+	if  i % 3 == 0 and i % 5 == 0:
+		print("Merry Christmas")
 	elif i % 5 == 0:
 		print("Christmas")
-	elif i % 3 == 0 and i % 5 == 0:  # The num '15' should print 'Merry Christmas'
-		print("Merry Christmas!") # but it only prints 'Merry'
+	elif i % 3 == 0:  # The num '15' should print 'Merry Christmas'
+		print("Merry") # but it only prints 'Merry'
 	else:
 		print(i)
+        
